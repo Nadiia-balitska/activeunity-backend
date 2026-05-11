f145dbf02e44618b7748e71dea903ec8bf74d987
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2026-05-11 16:39:07 +0200

    finished update and delete routes

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -4,17 +4,27 @@ const {
   createEvent,
   getAllEvents,
   getEventById,
+  updateEvent,
+  deleteEvent,
+  joinEvent,
+  leaveEvent,
+  getEventParticipants,
 } = require("../controllers/eventController");
 
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/")
-  .get(getAllEvents)
-  .post(protect, createEvent);
+router.route("/").get(getAllEvents).post(protect, createEvent);
 
-router.route("/:id")
-  .get(getEventById);
+router
+  .route("/:id")
+  .get(getEventById)
+  .put(protect, updateEvent)
+  .delete(protect, deleteEvent);
+
+router.post("/:id/join", protect, joinEvent);
+router.delete("/:id/leave", protect, leaveEvent);
+router.get("/:id/participants", getEventParticipants);
 
 module.exports = router;
