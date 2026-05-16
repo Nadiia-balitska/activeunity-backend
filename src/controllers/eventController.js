7836cd50b35664bd982d8adb7d5bfc0504ed6fa4
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2026-05-17 02:35:16 +0330

    sorting by nearest events.

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -15,42 +15,46 @@ const createEvent = asyncHandler(async (req, res) => {
  
 });
 
-const getAllEvents = asyncHandler(async (req, res)=> {
-  
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+const getAllEvents = asyncHandler(async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-    const query = {};
+  const query = {};
 
-    if (req.query.category) query.category = req.query.category;
-    if (req.query.status) query.status = req.query.status;
+  if (req.query.category) query.category = req.query.category;
+  if (req.query.status) query.status = req.query.status;
 
-    if (req.query.search) {
-      query.$or = [
-        { title: { $regex: req.query.search, $options: "i" } },
-        { description: { $regex: req.query.search, $options: "i" } },
-        { location: { $regex: req.query.search, $options: "i" } },
-      ];
-    }
+  if (!req.query.status) {
+    query.status = "upcoming";
+  }
 
-    const events = await Event.find(query)
-      .populate("organizer", "name email")
-      .sort({ date: 1 })
-      .skip(skip)
-      .limit(limit);
+  if (req.query.search) {
+    query.$or = [
+      { title: { $regex: req.query.search, $options: "i" } },
+      { description: { $regex: req.query.search, $options: "i" } },
+      { location: { $regex: req.query.search, $options: "i" } },
+    ];
+  }
 
-    const total = await Event.countDocuments(query);
+  const events = await Event.find(query)
+    .populate("organizer", "name email")
+    .populate("participants", "name email")
+    .sort({ date: 1 })
+    .skip(skip)
+    .limit(limit);
 
-    res.status(200).json({
-      success: true,
-      page,
-      pages: Math.ceil(total / limit),
-      total,
-      count: events.length,
-      events,
-    });
-})
+  const total = await Event.countDocuments(query);
+
+  res.status(200).json({
+    success: true,
+    page,
+    pages: Math.ceil(total / limit),
+    total,
+    count: events.length,
+    events,
+  });
+});
 
 const getEventById = asyncHandler(async (req, res) => {
     const event = await Event.findById(req.params.id)
